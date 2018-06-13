a62747279f1bc99629cf0293d480d9c1798d8c28
Andrea Gilardi <a.gilardi5@campus.unimib.it>
2018-06-13 10:24:53 +0200

    Change data import

--- data_import.R
+++ data_import.R
@@ -4,7 +4,11 @@ library(tidyverse)
 
 ### Data
 
+print("Inizio import training")
 train <- read_csv("data/TRAIN.txt")
+print("Fine import training")
+print("Inizio import test")
 test <- read_csv("data/TEST0.txt")
-
+print("fine import test")
+print("------------------------------------")
 ###
